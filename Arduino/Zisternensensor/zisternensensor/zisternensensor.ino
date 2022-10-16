65398295c5261b68e3235ef8010409d58b68ff4c
Michael Koch <tensberg@gmx.net>
2022-10-16 15:32:56 +0200

    Add zisternensensor Fritzing sketches
    Also update Ardunio sketch with pins used in the sketches.

--- Arduino/Zisternensensor/zisternensensor/zisternensensor.ino
+++ Arduino/Zisternensensor/zisternensensor/zisternensensor.ino
@@ -1,6 +1,7 @@
 #include "src/common/configuration.h"
 #include "src/common/secrets.h"
 
+// Wifi
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
@@ -13,32 +14,22 @@
 // Distance Sensor
 #include <NewPing.h>
 
-#define vcc3VPin 13
-#define vcc5VPin 15
+// Pin definitions
+#define VCC_3V_PIN 5
+#define VCC_5V_PIN 15
+#define DISTANCE_TRIG_PIN 14
+#define DISTANCE_ECHO_PIN 12
+#define I2C_SDA 0
+#define I2C_SCL 4
 
 // environment sensor
 #define BME_I2C_ADDRESS 0x76
-
 Adafruit_BME280 bme; // I2C
 
-// display sensor
-// Define Trig and Echo pin:
-#define trigPin 14
-#define echoPin 12
-
+// distance sensor
 // Define maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500 cm:
 #define MAX_DISTANCE 400
-
-NewPing sonar(trigPin, echoPin, MAX_DISTANCE);
-
-sensordata data;
-int message_counter = 0;
-
-struct send_status {
-  int sendSuccess;
-  int sendFailed;
-  int lastSendStatus;
-} send_status;
+NewPing sonar(DISTANCE_TRIG_PIN, DISTANCE_ECHO_PIN, MAX_DISTANCE);
 
 #define ENABLE_DISPLAY
 
@@ -51,51 +42,54 @@ struct send_status {
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// The pins for I2C are defined by the Wire-library.
-// On an arduino UNO:       A4(SDA), A5(SCL)
-// On an arduino MEGA 2560: 20(SDA), 21(SCL)
-// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3c ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3c
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #endif
 
+// zisternensensor data
+sensordata data;
+int message_counter = 0;
+
+struct send_status {
+  int sendSuccess;
+  int sendFailed;
+  int lastSendStatus;
+} send_status;
+
+
 void setup() {
   Serial.begin(57600);
-  Serial.println("\n\n\nHello Sensor");
+  Serial.println("\n\n\nsetup");
 
-  pinMode(vcc3VPin, OUTPUT);
-  digitalWrite(vcc3VPin, HIGH);
-  pinMode(vcc5VPin, OUTPUT);
-  digitalWrite(vcc5VPin, HIGH);
+  pinMode(VCC_3V_PIN, OUTPUT);
+  pinMode(VCC_5V_PIN, OUTPUT);
+  sensorsOn();
 
+  Wire.pins(I2C_SDA, I2C_SCL);
   initEspNow();
   initBme();
   initDisplay();
 }
 
-int loop_counter = 0;
-
 void loop() {
-  if ((loop_counter++ % 100) == 0) {
-    data.temperature = bme.readTemperature();
-    data.pressure = bme.readPressure() / 100.0F;
-    data.humidity = bme.readHumidity();
-    data.distance = sonar.convert_cm(sonar.ping_median());
-    //data.distance = message_counter++; // for testing only
+  data.temperature = bme.readTemperature();
+  data.pressure = bme.readPressure() / 100.0F;
+  data.humidity = bme.readHumidity();
+  data.distance = sonar.convert_cm(sonar.ping_median());
   
-    int res = esp_now_send(NULL, (uint8_t *) &data, sizeof(data));
-    if (res != 0) {
-      Serial.print("send failed: ");
-      Serial.println(res);
-    }
-
-    displayData();
+  int res = esp_now_send(NULL, (uint8_t *) &data, sizeof(data));
+  if (res != 0) {
+    Serial.print("send failed: ");
+    Serial.println(res);
   }
 
+  displayData();
 
-  delay(50);
+  sensorsOff();
+  ESP.deepSleep(5e6);
+  // we should never get here because deepSleep is ended with a reset
 }
 
 void initEspNow() {
@@ -201,9 +195,20 @@ void displayData() {
   display.println(send_status.sendSuccess);
   display.print("send failed: ");
   display.println(send_status.sendFailed);
-
-  //digitalWrite(vcc5VPin, send_status.sendFailed%2 == 0 ? HIGH : LOW);
   
   display.display();
 #endif
+}
+
+void sensorsOn() {
+  switchSensorsVcc(HIGH);
+}
+
+void sensorsOff() {
+  switchSensorsVcc(LOW);
+}
+
+void switchSensorsVcc(int value) {
+  digitalWrite(VCC_3V_PIN, value);
+  digitalWrite(VCC_5V_PIN, value);
 }
